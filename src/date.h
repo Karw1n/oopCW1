86cf13237efdc5c2919e320099fd9d9832cff848
Karw1n <akbbalfour@gmail.com>
2024-03-07 10:18:15 +0000

    Initial commit

---
+++ src/date.h
@@ -0,0 +1,42 @@
+// -----------------------------------------------------
+// CSC371 Advanced Object Oriented Programming (2023/24)
+// Department of Computer Science, Swansea University
+//
+// Author: <STUDENT NUMBER>
+//
+// Canvas: https://canvas.swansea.ac.uk/courses/44636
+// -----------------------------------------------------
+// An Date class that contains the following member variables
+// - year
+// - month
+// - day
+// - initialised
+//
+// A Date object should only be set to initialised if it is a valid date.
+// Otherwise it is should remain unintialised.
+// -----------------------------------------------------
+
+#ifndef DATE_H
+#define DATE_H
+
+#include "lib_json.hpp"
+
+class Date {
+private:
+  unsigned int year, month, day;
+  bool initialized;
+
+public:
+  Date();
+  void setDateString(std::string string);
+  bool isInitialised() const;
+  std::string str() const;
+  void setDate(unsigned int year, unsigned int month, unsigned int day);
+  unsigned int getYear() const;
+  unsigned int getMonth() const;
+  unsigned int getDay() const;
+  bool operator==(const Date &otherDate) const;
+  bool operator<(const Date &otherDate) const;
+};
+
+#endif // DATE_H
